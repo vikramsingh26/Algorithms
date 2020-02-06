ea83b36a57a45f477b7af0bb97f3a954862683e5
vsingh316 <vikramsingh@gatech.edu>
2020-02-05 20:04:05 -0500

    Fibonnaci done

--- C++/Fibonnaci.cpp
+++ C++/Fibonnaci.cpp
@@ -8,9 +8,11 @@
 
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <map>
 using namespace std;
 
-double fibonnaci_version1(double number)
+double fibonnaci_version1(int number)
 {
     if(number < 1)
         return 0;
@@ -20,33 +22,53 @@ double fibonnaci_version1(double number)
     return fibonnaci_version1(number -1) + fibonnaci_version1(number - 2);
 }
 
+map<int, double> g_map;
+double fibonnaci_version2(int number)
+{
+    if(g_map.find(number) != g_map.end())
+    {
+        return g_map[number];
+    }
+    double value = 0;
+    if((number == 0) or (number == 1))
+    {
+        value = number;
+    }
+    else if(number > 1)
+    {
+        value = fibonnaci_version2(number - 1) + fibonnaci_version2(number - 2);
+    }
+
+    g_map[number]  = value;
+    return value;
+}
+
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
-    
+   
     do {
         int number ;
         cout << "enter the number to calculate fibannaci or type q for quitting\n";
         cin >> number;
-        
-        if(to_string(number) == "q")
-            cout << " quitting";
-            exit(0);
-        
-        if(isdigit(number))
+        if(cin.fail())
         {
-            auto start = std::chrono::system_clock::now();
-
-            auto result =  fibonnaci_version1(number);
-            std::chrono::duration<double> time_took = std::chrono::system_clock::now() - start; 
-            cout << "Result = " << result << "\nTime Took by Version1 = " << time_took.count();
+            cin.clear();
+            cout << "Invalid failed \n";
+            break;
         }
-        else
-        {
-            cout << "Not a valid number = " << number;
-        }
+        cout << "Number = " << number << endl;
         
+        auto start = std::chrono::system_clock::now();
 
+        auto result =  fibonnaci_version1(number);
+        std::chrono::duration<double> time_took = std::chrono::system_clock::now() - start; 
+        cout << "Result = " << result << "\nTime Took by Version1 = " << time_took.count() << " seconds"<< endl;
+        start = std::chrono::system_clock::now();
+
+        result = fibonnaci_version2(number);
+        time_took = std::chrono::system_clock::now() - start;
+        cout << "Result = " << result << "\nTime Took by Version2 = " << time_took.count() << " seconds"<< endl;
+        
     }
     while(true);
 
